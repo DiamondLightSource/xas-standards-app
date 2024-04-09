58e7c73baebc827a4197f0a919d6a2db4137748b
Jacob Filik <jacob.filik@gmail.com>
2024-04-09 09:38:11 +0100

    Basic storybook structure

---
+++ xas-standards-client/src/stories/StandardChart.stories.ts
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import XASChart from "../components/StandardsChart";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "XAS-Standards/XASChart",
+  component: XASChart,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  //   argTypes: {
+  //     onClickElement: { control: "color" },
+  //     elementSize: number,
+  //   },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof XASChart>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    xasdata: null,
+    showTrans: false,
+    showFluor: false,
+    showRef: false,
+    contains: [false, false, false],
+  },
+};
